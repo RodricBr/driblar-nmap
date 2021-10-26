caf113401f3f57e8f64b9d527ed6999a3c94f64b
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-10-26 19:32:32 -0300

    Update dibre.sh

--- dibre.sh
+++ dibre.sh
@@ -3,8 +3,6 @@
 echo -e '\x1b[32m1 - DIBRAR\n2 - VOLTAR AO NORMAL\n\n\x1b[00m'
 read -rp "Opção: " opc_
 
-echo -e "Comando NMAP:\nsudo nmap --script=qscan 127.0.0.1 -n -vvv --ttl=128 -Pn --reason --top-ports=10"
-
 if [[ "$opc_" == "1" ]]; then
   sudo iptables -P INPUT DROP && sudo iptables -F INPUT
   # parte nova:
@@ -19,3 +17,6 @@ else
   echo -e "iptables não instalado no seu sistema!\n"
   exit 1
 fi
+
+echo -e "Para testar, rode esse comando:"
+echo -e '\x1b[32msudo nmap --script=qscan 127.0.0.1 -n -vvv --ttl=128 -Pn --reason --top-ports=10\x1b[00m\n'
